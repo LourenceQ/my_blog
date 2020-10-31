0807e981dcf7f8dc4f37524bad121c872d484a69
Louernce <lqfmecatronica@gmail.com>
2020-10-31 14:22:28 -0300

    Implementation of custom template tags

---
+++ blog_pro/blog_app/templatetags/blog_tags.py
@@ -0,0 +1,20 @@
+from django import template
+from ..models import Post
+from django.db.models import Count
+
+register = template.Library()
+
+@register.simple_tag
+def total_posts():
+    return Post.publicado.count()
+
+@register.inclusion_tag('blog_app/post/ultimos_posts.html')
+def mostrar_ultimos_posts(count=5):
+    ultimos_posts = Post.publicado.order_by('-publicar')[:count]
+    return {'ultimos_posts': ultimos_posts}
+
+@register.simple_tag
+def get_postagens_mais_comentadas(count=5):
+    return Post.publicado.annotate(
+                total_commentarios=Count('comentarios')
+                ).order_by('-total_commentarios')[:count]
